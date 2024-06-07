c4aa32a44e1355ebe2ecf3b0886e19ce0a87ae37
harsha-0210 <118594956+harsha-0210@users.noreply.github.com>
2024-06-07 20:32:48 +0530

    Add files via upload

---
+++ lecture5.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int a,b;
+	int c,d;
+	
+	cout<<"Input 2 integers a,b,c and d"<<endl;
+	
+	cin>>a;
+	cin>>b;
+	cin>>c;
+	cin>>d;
+}	
+	// Statement :a>b
+	cout<<(a>b && c>d);
+	cout<<(a>b || c>d); 
+
+// execute AND and OR statements and print out the boolean:
